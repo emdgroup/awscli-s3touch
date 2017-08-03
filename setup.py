8b6ae6dd1005e7d22a1e11cc858cf91a4de12b42
Moritz Onken <onken@netcubed.de>
2017-08-03 07:48:45 -0400

    fix installation issues with namespaces

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import find_packages, setup
 setup(
     name = 'awscli-s3touch',
-    packages = find_packages(),
-    version = '0.0.1',
+    description='Simulate S3 events without re-uploading a file.',
+    long_description=open('README.rst').read(),
+    packages = ['awscli.plugins.s3touch'],
+    version = '0.0.6',
     install_requires=['awscli'],
     author='Merck Group',
     url='https://github.com/merckgroup/awscli-s3touch',
     license='Apache License 2.0',
     python_requires='>=3',
-    description = 'Simulate S3 events without re-uploading a file.',
-    namespace_packages = ['awscli'],
+    package_data={'': ['examples/*.rst']},
 )
